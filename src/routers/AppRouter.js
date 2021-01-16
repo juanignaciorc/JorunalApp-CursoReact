4d288336e7531f32694f2cd300d515c5d79e66f4
Juan Ignacio Roldan <juanignacioroldan01@gmail.com>
2021-01-16 02:14:35 -0300

    login y logout updated

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import {firebase} from '../firebase/firebase-config'
 import {
     BrowserRouter as Router,
     Switch,
@@ -6,10 +7,41 @@ import {
     Redirect
   } from 'react-router-dom';
 
+import {useDispatch} from 'react-redux';
+
 import { AuthRouter } from './AuthRouter';
 import { JournalScreen } from '../components/journal/JournalScreen';
+import { login } from '../actions/auth';
+import { useState } from 'react';
 
 export const AppRouter = () => {
+
+    const dispatch = useDispatch();
+
+    const [checking, setChecking] = useState(true);
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+    useEffect(() => {
+
+        firebase.auth().onAuthStateChanged( (user) => {    
+
+            if(user?.uid){
+                dispatch( login( user.uid, user.displayName ) );
+                setIsLoggedIn(true);
+            }
+
+            setChecking(false);
+        });
+    }, [ dispatch, setChecking ])
+
+    if (checking) {
+        return(
+            <h1>Loading... Please wait  </h1>
+        )
+        
+    }
+
     return (
         <Router>
             <div>
